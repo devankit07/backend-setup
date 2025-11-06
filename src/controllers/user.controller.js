50deda9d5aec40673652ece1706c6abd2af7e7ea
Ankitkumar277 <Ankitrathor272005@gmail.com>
2025-11-06 15:15:31 +0530

    add regiser controller & bug fix

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,74 @@
 import {asynchandler} from "../utils/asynchandler";
+import{apierror}from "../utils/apierrors.js"
+import{user} from "../models/user.model.js";
+import{uploadCloudinary} from"../utils/cloudinary.js";
+import{apiresponse} from "../utils/apiresponse.js";
 
 const registeruser = asynchandler(async(request, response)=>{
-   return response.status(200).json({
-        message:"ok"
-    });
+   //get user details from forntend
+   //validation - not empty
+   //check if already exists: username , email
+   //check for images & for avatar
+   // upload them to cloudinary, avatar
+   // create user object - crrate entry in db
+   // remove password and refresh token field from response
+   // check for user creation
+   // response return
+
+   const{fullname, email, username,password} = request.body
+   console.log("email:",email);
+
+   if (
+    [fullname,email,password,username].some((field)=>
+        field?.trim()==="")
+   ){
+    throw new apierror(400, "all fields are required");
+   }
+
+   const existing = user.findOne({
+    $or:[{username},{email}]
+   })
+
+   if(existing){
+    throw new apierror(409, "user with email or sername already exists")
+   }
+
+   const avatarlocalpath = request.files?.avatar[0]?.path;
+   const coverimagelocalpath = request.files?.coverimage[0]?.path;
+
+   if(!avatarlocalpath){
+    throw new  apierror(400, "avatar file is required")
+   }
+
+
+   const avatar =await uploadCloudinary(avatarlocalpath)
+   const coverimage = await uploadCloudinary(coverimagelocalpath)
+
+   if(!avatar){
+    throw new apierror("400", "avatar file is required")
+   }
+
+   const user  =await user.create({
+    fullname,
+    avatar:avatar.url,
+    coverimage: coverimage?.url || "",
+    email,
+    password,
+    username: username.tolowercase()
+   })
+
+   const createuser =user.findbyid(user._id).select(
+    "-password -refreshToken"
+   );
+
+   if(!createuser){
+    throw new apierror("500", "sometehting wrong ")
+   }
+
+   return response.status(201).json(
+    new apiresponse(200, createuser, "user refistered succesfully")
+   )
+
 });
 
 export{registeruser}
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,24 @@
 import { Router } from "express";
 import { registeruser } from "../controllers/user.controller.js"; // 👈 include .js
 
+
+import{upload} from "../middlewares/multer.middleware.js"
 const router = Router();
 
-router.route("/register").post(registeruser);
+router.route("/register").post(
+    upload.fields([
+        {
+            name: "avatar",
+            maxcount: 1
+        },
+
+        {
+            name:"coverimage",
+            maxCount: 1
+        }
+
+    ]),
+    registeruser
+);
 
 export default router;
